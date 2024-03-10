6a40ab744f21c933b0abd3f172948d4dce551185
spotverma2194 <deepakverm0522@gmail.com>
2024-03-10 23:38:36 +0530

    Bubble sort program

--- Bubblesort/src/main/java/main.java
+++ Bubblesort/src/main/java/main.java
@@ -14,6 +14,24 @@ public class main {
             }
         }
     }
+    //optimize bubble sort
+    static void bubblesort2(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            boolean flag=false; // swapping not happened
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                    flag=true;// same swap has happened
+                }
+            }
+            if(!flag){ //ans swap does not happened
+                return;
+            }
+        }
+    }
     public  static void  main(String[] aargs){
         int[] num={5,7,4,3,1};
         bubblesort(num);
