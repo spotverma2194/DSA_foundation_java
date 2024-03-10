8e73d9f07ad2bb7d58e528b490a5d78965ab35a5
spotverma2194 <deepakverm0522@gmail.com>
2024-03-10 22:10:59 +0530

    Bubble sort program

--- Bubblesort/src/main/java/main.java
+++ Bubblesort/src/main/java/main.java
@@ -1,2 +1,22 @@
+import java.util.Arrays;
+
 public class main {
+
+    static void bubblesort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                   int tmp=arr[j];
+                   arr[j]=arr[j+1];
+                   arr[j+1]=tmp;
+                }
+            }
+        }
+    }
+    public  static void  main(String[] aargs){
+        int[] num={5,7,4,3,1};
+        bubblesort(num);
+        System.out.println(Arrays.toString(num));
+    }
 }
