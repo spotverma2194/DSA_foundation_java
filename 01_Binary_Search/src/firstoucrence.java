f6c1ef3ebdc74994bb20b9f49fdd7e0d0f5dbd7e
deepak <deepakverma0522@gmail.com>
2024-06-29 17:22:18 +0530

    First Oucrrence using binary search

---
+++ 01_Binary_Search/src/firstoucrence.java
@@ -0,0 +1,25 @@
+public class firstoucrence {
+    static int firstoccbinary(int[] a,int x ){
+        int n=a.length;
+        int fo=-1;
+        int st=0;
+        int ed=n-1;
+        while(st<=ed){
+            int mid=st+(ed-st)/2;
+            if(x==a[mid]){
+                fo=mid;
+                ed=mid-1;
+            } else if (x<a[mid]) {
+                ed=mid-1;
+            }else{
+                st=mid+1;
+            }
+        }
+        return fo ;
+    }
+    public static void main(String[] args){
+        int[] arr={2,2,2,3,4,4,5,5,5,6,7,8,8,8};
+        int x=5;
+        System.out.println(firstoccbinary(arr,x));
+    }
+}
