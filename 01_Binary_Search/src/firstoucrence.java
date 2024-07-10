287f4c5d66e6fca6723abcc74731b4603fe786f1
deepak <deepakverma0522@gmail.com>
2024-07-10 12:03:47 +0530

    Some modification

--- 01_Binary_Search/src/firstoucrence.java
+++ 01_Binary_Search/src/firstoucrence.java
@@ -19,7 +19,7 @@ public class firstoucrence {
     }
     public static void main(String[] args){
         int[] arr={2,2,2,3,4,4,5,5,5,6,7,8,8,8};
-        int x=5;
+        int x=3;
         System.out.println(firstoccbinary(arr,x));
     }
 }
